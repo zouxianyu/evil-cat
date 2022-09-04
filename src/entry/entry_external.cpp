6e1689618f30476a22e7eabe7d33092e7977bc75
zouxianyu <2979121738@qq.com>
2022-09-04 16:14:18 +0800

    fix: fix internal and external entry

--- src/entry/entry_external.cpp
+++ src/entry/entry_external.cpp
@@ -1,8 +1,11 @@
-#include <thread>
 #include "service/entry.h"
 
+// this is the external entry
+// if we compile it as an executable file
+// the 'main' is the entry of the executable file
+// we call that 'external entry'
 int main(int argc, char *argv[]) {
-    std::thread t(entry);
-    t.join();
+    // just call the real entry
+    entry();
     return 0;
 }
--- src/entry/entry_internal.cpp
+++ src/entry/entry_internal.cpp
@@ -2,28 +2,42 @@
 #include <windows.h>
 #include "service/entry.h"
 
-// alloc console for injected dll if needed
+// if we inject the dll into the target process
+// and when we want to exit, we need to free the library
 static void entryWrapper(HMODULE hModule) {
+
+    // allocate a console for internal usage
 #if (CONF_USING_STD_CONSOLE == 1)
     AllocConsole();
     FILE *f;
     freopen_s(&f, "CONOUT$", "w", stdout);
 #endif
 
+    // call the real entry
     entry();
 
+    // free the console
 #if (CONF_USING_STD_CONSOLE == 1)
     fclose(f);
     FreeConsole();
-    FreeLibraryAndExitThread(hModule, 0);
 #endif
+
+    // free the library
+    // so we can inject the dll to the target process again
+    FreeLibraryAndExitThread(hModule, 0);
 }
 
+// DllMain is the entry point of the dll
+// we call that 'internal entry'
 BOOL APIENTRY DllMain(HMODULE hModule,
                       DWORD ul_reason_for_call,
                       LPVOID lpReserved
 ) {
+    // if the dll is injected into the target process
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
+
+        // create a thread and detach from it
+        // so the DllMain can return immediately
         std::thread t(entryWrapper, hModule);
         t.detach();
     }
