23f994299a14639fd02dfa95487b1f40548a21cf
zouxianyu <2979121738@qq.com>
2022-09-06 23:11:56 +0800

    feature: implement world to screen in the framework

--- src/service/world_to_screen/world_to_screen.cpp
+++ src/service/world_to_screen/world_to_screen.cpp
@@ -1,42 +1,46 @@
-#include <imgui/imgui.h>
-#include "entity/entity_manager.h"
-#include "entity/offset/offset.h"
+#include <memory>
+#include <imgui.h>
+#include "game.h"
 #include "mem/process_memory_accessor.h"
-
 #include "world_to_screen.h"
-#include "game_matrix_world_to_screen.h"
-#include "self_matrix_world_to_screen.h"
 
 WorldToScreen &WorldToScreen::getInstance() {
-#if (CONF_USE_GAME_VIEW_MATRIX == 1)
-    return GameMatrixWorldToScreen::getInstance();
-#else
-    return SelfMatrixWorldToScreen::getInstance();
-#endif
+    static WorldToScreen instance;
+    return instance;
 }
 
 bool WorldToScreen::refresh() {
-    vpMatrix = getViewProjectionMatrix();
-//    width = ImGui::GetWindowWidth();
-//    height = ImGui::GetWindowHeight();
-    width = 1024;
-    height = 768;
+
+    // refresh view projection matrix
+    std::shared_ptr<Mat4> mat = Game::getInstance().getVPMatrix();
+    vpMatrix = {
+            mat->m[0][0], mat->m[0][1], mat->m[0][2], mat->m[0][3],
+            mat->m[1][0], mat->m[1][1], mat->m[1][2], mat->m[1][3],
+            mat->m[2][0], mat->m[2][1], mat->m[2][2], mat->m[2][3],
+            mat->m[3][0], mat->m[3][1], mat->m[3][2], mat->m[3][3]
+    };
+
+    // refresh window size
+    Vec2 windowSize = Game::getInstance().getWindowSize();
+    width = windowSize.x;
+    height = windowSize.y;
+
     return true;
 }
 
-bool WorldToScreen::translate(const Vec3 &world, Vec2 &screen) {
+std::optional<Vec2> WorldToScreen::translate(const Vec3 &world) {
 
+    // calculate clip coordinates
     glm::vec4 clip = vpMatrix * glm::vec4{world.x, world.y, world.z, 1.0f};
     if (clip.w < 0.1f) {
-        return false;
+        return std::nullopt;
     }
+
+    // calculate NDC coordinates
     glm::vec4 NDC = clip / clip.w;
-    Vec2 screenTry;
-    screenTry.x = (NDC.x + 1.0f) * 0.5f * width;
-    screenTry.y = (-NDC.y + 1.0f) * 0.5f * height;
-    if (screenTry.x < -width || screenTry.x >= 2*width || screenTry.y < -height || screenTry.y >= 2*height) {
-        return false;
+    Vec2 screen{(NDC.x + 1.0f) * 0.5f * width, (-NDC.y + 1.0f) * 0.5f * height};
+    if (screen.x < -width || screen.x >= 2 * width || screen.y < -height || screen.y >= 2 * height) {
+        return std::nullopt;
     }
-    screen = screenTry;
-    return true;
+    return screen;
 }
--- src/service/world_to_screen/world_to_screen.h
+++ src/service/world_to_screen/world_to_screen.h
@@ -1,23 +1,22 @@
 #ifndef ASSAULT_CUBE_HACKING_WORLD_TO_SCREEN_WORLD_TO_SCREEN_H
 #define ASSAULT_CUBE_HACKING_WORLD_TO_SCREEN_WORLD_TO_SCREEN_H
 
+#include <optional>
 #include <functional>
 #include <glm/glm.hpp>
 #include "type.h"
 
 class WorldToScreen {
-    glm::mat4 vpMatrix{}; // cached view matrix
-    float width{};
-    float height{};
-
-    virtual glm::mat4 getViewProjectionMatrix() = 0;
+    glm::mat4 vpMatrix; // cached view matrix
+    float width;
+    float height;
 
 public:
     static WorldToScreen &getInstance();
 
     bool refresh();
 
-    bool translate(const Vec3 &world, Vec2 &screen);
+    std::optional<Vec2> translate(const Vec3 &world);
 };
 
 #endif //ASSAULT_CUBE_HACKING_WORLD_TO_SCREEN_WORLD_TO_SCREEN_H
