356359acc073b5686c6d78465570617c4a823c1d
zouxianyu <2979121738@qq.com>
2022-09-07 17:58:46 +0800

    feature: change menu, and bind all settings in each module to it

--- src/game/assault_cube/menu.cpp
+++ src/game/assault_cube/menu.cpp
@@ -1,4 +1,9 @@
 #include <imgui.h>
+#include "service/aimbot/aimbot.h"
+#include "service/esp/esp.h"
+#include "service/name_list/name_list.h"
+#include "service/health_locker/health_locker.h"
+
 #include "settings.h"
 #include "menu.h"
 
@@ -12,23 +17,26 @@ void Menu::show() {
     ImGui::Text("Hacking Template");
 
     //show esp toggle
-    ImGui::Checkbox("ESP", &Settings::getInstance().showEsp);
-    static int ESPSelection = 0;
-    ImGui::SameLine();
-    ImGui::RadioButton("2D", &ESPSelection, 0);
-    ImGui::SameLine();
-    ImGui::RadioButton("3D", &ESPSelection, 1);
-    Settings::getInstance().showEspBox2D = (ESPSelection == 0);
+    ImGui::Checkbox("ESP", &Settings::Esp::on);
 
-    ImGui::Checkbox("show health", &Settings::getInstance().showHealthAndArmor);
+    ImGui::SameLine();
+    ImGui::RadioButton("2D", (int *) &Settings::Esp::boxType, (int) Settings::Esp::BoxType::show2D);
+    ImGui::SameLine();
+    ImGui::RadioButton("3D", (int *) &Settings::Esp::boxType, (int) Settings::Esp::BoxType::show3D);
 
     //show aimbot
-    ImGui::Checkbox("aimbot (on shooting)", &Settings::getInstance().aimbot);
-    ImGui::SliderAngle("max aimbot angle", &Settings::getInstance().aimbotMaxAngle, 0.f, 180.f);
+    ImGui::Checkbox("aimbot", &Settings::Aimbot::on);
+    ImGui::SliderAngle("max aimbot angle", &Settings::Aimbot::maxAngle, 0.f, 180.f);
+
+    // show name list options
+    ImGui::Checkbox("name list", &Settings::NameList::on);
+
+    // show health locker options
+    ImGui::Checkbox("health locker", &Settings::HealthLocker::on);
 
     // show exit button
     if (ImGui::Button("exit")) {
-        Settings::getInstance().exit = true;
+        Settings::exit = true;
     }
 
     ImGui::End();
