169acd64b61ae3823a2a9e9e5c8a36b48e55ff74
zouxianyu <2979121738@qq.com>
2022-10-24 10:55:33 +0800

    allocate a debug console for the external mode if needed

--- src/entry/entry_external.cpp
+++ src/entry/entry_external.cpp
@@ -1,6 +1,8 @@
 #ifndef CONF_INTERNAL_COMPILE
 
+#include <windows.h>
 #include <dwmapi.h>
+#include <stdio.h>
 #include "entry/entry.h"
 
 // this is the external entry
@@ -9,8 +11,19 @@
 // we call that 'external entry'
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     SetProcessDPIAware();
+    // allocate a console for internal usage
+#if (CONF_USING_STD_CONSOLE == 1)
+    AllocConsole();
+    FILE *f;
+    freopen_s(&f, "CONOUT$", "w", stdout);
+#endif
     // just call the real entry
     entry();
+    // free the console
+#if (CONF_USING_STD_CONSOLE == 1)
+    fclose(f);
+    FreeConsole();
+#endif
     return 0;
 }
 
